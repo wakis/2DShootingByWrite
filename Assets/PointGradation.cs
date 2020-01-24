c305bc64261fadb79c0897291702a49f71b247ef
Wakis <s1723085@cce.kanagawa-it.ac.jp>
2020-01-24 20:43:14 +0900

    UIの制作

---
+++ Assets/PointGradation.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointGradation : MonoBehaviour
+{
+    Color defCol = new Color();
+    bool colAlphaToOne;
+    public bool onTriggerInObj;
+    private void Awake()
+    {
+        defCol = GetComponent<SpriteRenderer>().color;
+        onTriggerInObj = false;
+        colAlphaToOne = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (colAlphaToOne)
+        {
+            defCol.a += Time.deltaTime * 0.8f;
+            if (defCol.a>=1f)
+            {
+                defCol.a = 1f;
+                colAlphaToOne = !colAlphaToOne;
+            }
+        }
+        else
+        {
+            defCol.a -= Time.deltaTime * 0.8f;
+            if (defCol.a <= 0f)
+            {
+                defCol.a = 0f;
+                colAlphaToOne = !colAlphaToOne;
+            }
+        }
+        if (GetComponent<SpriteRenderer>().color != defCol){
+            GetComponent<SpriteRenderer>().color = defCol;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!onTriggerInObj)
+        {
+            if (collision.tag == "Player")
+            {
+                onTriggerInObj = !onTriggerInObj;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (onTriggerInObj)
+        {
+            if (collision.tag == "Player")
+            {
+                onTriggerInObj = !onTriggerInObj;
+            }
+        }
+    }
+}
---
+++ Assets/setTextFront.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setTextFront : MonoBehaviour
+{
+    [SerializeField, ContextMenuItem("SetLayer", "SetLayer")]
+    string layerName;
+    [SerializeField,ContextMenuItem("SetLayer", "SetLayer")]
+    int sortingNum;
+    void SetLayer()
+    {
+        GetComponent<Renderer>().sortingLayerName = layerName;
+        GetComponent<Renderer>().sortingOrder = sortingNum;
+    }
+
+}
